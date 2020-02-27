0f32f7c00148f7cd931191a8fcebd52c29ce1fcd
mrkwapo <mrkwapo@gmail.com>
2020-02-27 10:28:45 -0500

    completed CRD

--- public/scripts.js
+++ public/scripts.js
@@ -1,43 +1,4 @@
-const sendHttpRequest = (method, url, data) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open(method, url);
-
-    xhr.responseType = "json";
-
-    if (data) {
-      xhr.setRequestHeader("Content-Type", "application/json");
-    }
-
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
-        resolve(xhr.response);
-      }
-    };
-
-    xhr.onerror = () => {
-      reject("Something went wrong!");
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
-};
-
-const getAllGraduates = () => {
-  sendHttpRequest("GET", "http://localhost:3000/api/graduates", true).then(
-    responseData => {
-      console.log(responseData);
-      document.getElementById("results").innerHTML = JSON.stringify(
-        responseData
-      );
-    }
-  );
-};
-
+//CREATE
 function createNewGraduate() {
   event.preventDefault();
   var graduate = {
@@ -51,4 +12,94 @@ function createNewGraduate() {
   xhr.open("POST", "http://localhost:3000/api/graduates");
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(JSON.stringify(graduate));
+}
+
+//READ All
+function getAllGraduates() {
+  var url = "http://localhost:3000/api/graduates";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function() {
+    var graduates = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      console.log(graduates);
+      document.getElementById("results").innerHTML = JSON.stringify(graduates);
+    } else {
+      console.error(graduates);
+    }
+  };
+  xhr.send(null);
+}
+
+//READ One (by Email)
+function getOneGraduateByEmail() {
+  event.preventDefault();
+  const input = document.getElementById("findByEmail").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:3000/api/graduates/" + input, true);
+  console.log(input);
+
+  xhr.onload = function() {
+    console.log(JSON.parse(xhr.responseText));
+    var graduate = JSON.stringify(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      console.log(graduate);
+      document.getElementById("emailResult").innerHTML = graduate;
+    } else {
+      document.getElementById("emailResult").innerHTML =
+        "<i>graduate not found</i>";
+      console.error(graduate);
+    }
+  };
+
+  xhr.send(null);
+}
+
+//UPDATE
+function updateGraduate() {
+  event.preventDefault();
+  const email = document.getElementById("updateByEmail").value;
+  var updatedGraduate = {};
+
+  (updatedGraduate.firstName = document.getElementById(
+    "updateFirstName"
+  ).value),
+    (updatedGraduate.lastName = document.getElementById(
+      "updateLastName"
+    ).value);
+
+  var json = JSON.stringify(updatedGraduate);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("PUT", "http://localhost:3000/api/graduates/" + email, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  xhr.onload = function() {
+    var graduate = JSON.stringify(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      console.log(graduate);
+    } else {
+      console.error(graduate);
+    }
+  };
+  xhr.send(json);
+}
+
+//DELETE
+
+function deleteGraduate() {
+  event.preventDefault();
+  const input = document.getElementById("delete").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "http://localhost:3000/api/graduates/" + input, true);
+  xhr.onload = function() {
+    var graduate = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      document.getElementById("deleteResult").innerHTML =
+        "<i>Graduate has been deleted</i>";
+      console.log(graduate);
+    } else {
+      console.error(graduate);
+    }
+  };
+  xhr.send(null);
 }
